1f8662d7ef72c331103d119f7afd19845182fb45
tahsif <tahsif.cse@gmail.com>
2023-10-20 18:45:14 +0600

    create POST method for create Product

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 7000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-// console.log(process.env.DB_USER)
-// console.log(process.env.DB_PASS)
 
-
+const productDB = "BrandShop"; // Database name
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhtgohj.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,25 +24,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
+    console.log("Connected to MongoDB!");
+    const productCollection = client.db(productDB).collection("product");
+    
+    app.post('/product', async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+  } catch (error) {
+    console.error(error);
   }
 }
-run().catch(console.dir);
 
+run().then(() => {
+  app.get('/', (req, res) => {
+    res.send("Product like movie");
+  });
 
-
- 
-app.get('/', (req, res)=>{
-    res.send("Product like movie")
-})
-
-app.listen(port, ()=>{
-    console.log(`Product server:${port}`)
-})
+  app.listen(port, () => {
+    console.log(`Product server:${port}`);
+  });
+}).catch(console.dir);
