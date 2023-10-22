f32d74900d5220dc07c38f2a6df833f1b81acf57
tahsif <tahsif.cse@gmail.com>
2023-10-23 00:05:43 +0600

    Added Mycart POST and GET Method

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-   
+const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 7000;
 
-
-
-// middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -31,45 +31,67 @@ async function run() {
     await client.connect();
     console.log("Connected to MongoDB!");
     const productCollection = client.db(productDB).collection("product");
-    
-    app.get('/products/', async (req, res) => {
-        const cursor = productCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    const cartCollection = client.db(productDB).collection("cart");
 
-    })
-    app.post('/products/', async (req, res) => {
+    // Routes for product collection
+    app.get('/products', async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post('/products', async (req, res) => {
       const newProduct = req.body;
       console.log(newProduct);
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
     });
-    app.put('/products/:id', async (req, res)=>{
-        const id = req.params.id;
-        console.log('Received ID:', id); 
-        const filter = {_id: new ObjectId(id)}
-        const options = {upsert: true};
-        const updateProduct =req.body;
-        const product = {
-            $set: {
-                name: updateProduct.name,
-                brand: updateProduct.brand,
-                type: updateProduct.type,
-                price: updateProduct.price,
-                rating: updateProduct.rating,
-                des: updateProduct.des,
-                image: updateProduct.image
-            }
+
+    app.put('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log('Received ID:', id);
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateProduct = req.body;
+      const product = {
+        $set: {
+          name: updateProduct.name,
+          brand: updateProduct.brand,
+          type: updateProduct.type,
+          price: updateProduct.price,
+          rating: updateProduct.rating,
+          des: updateProduct.des,
+          image: updateProduct.image
         }
-        const result = await productCollection.updateOne(filter, product, options);
-        res.send(result);
-    })
+      };
+      const result = await productCollection.updateOne(filter, product, options);
+      res.send(result);
+    });
+
     app.get('/products/:id', async (req, res) => {
       const cursor = productCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    });
 
-  })
+    // Routes for cart collection
+   
+    app.post('/mycart/:email', async (req, res) => {
+      // const email = req.params.email;
+      // console.log('Received Email:', email);
+      const newCart = req.body;
+      console.log(newCart);
+      const result = await cartCollection.insertOne(newCart);
+      res.send(result);
+    });
+    app.get('/mycart/:email', async (req, res) => {
+      const email = req.params.email;
+      const cursor = cartCollection.find({email:email});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -84,6 +106,6 @@ run().then(() => {
   });
 
   app.listen(port, () => {
-    console.log(`Product server:${port}`);
+    console.log(`Product server listening on port ${port}`);
   });
-}).catch(console.dir);
+}).catch(console.error);
